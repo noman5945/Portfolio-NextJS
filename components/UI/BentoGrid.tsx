4a8828079ee14a7e6c886384058f92f31bec9afb
noman5945 <abdullahnoman.5945@gmail.com>
2024-06-25 21:47:27 +0600

    [WORK] Replace globe with anything

--- components/UI/BentoGrid.tsx
+++ components/UI/BentoGrid.tsx
@@ -8,6 +8,8 @@ export const BentoGrid = ({
   className?: string;
   children?: React.ReactNode;
 }) => {
+  //grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto
+  //origin: grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto
   return (
     <div
       className={cn(
@@ -51,8 +53,8 @@ export const BentoGridItem = ({
           "linear-gradient(90deg,rgba(2,0,36.1) 0%, rgba(59,59,68,1) 26%, rgba(93,108,111,1) 100%)",
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full`}>
-        <div className=" w-fit h-fit absolute">
+      <div className={`${id === 6 && "flex flex-col justify-center"} h-full`}>
+        <div className=" w-full h-full absolute">
           {img && (
             <img
               src={img}
@@ -63,7 +65,7 @@ export const BentoGridItem = ({
         </div>
         <div
           className={`absolute right-0 -bottom-5 ${
-            id === 5 && "w-full opacity-80"
+            id === 5 && " w-full opacity-80"
           } `}
         >
           {spareImg && (
@@ -75,21 +77,21 @@ export const BentoGridItem = ({
             />
           )}
         </div>
-        {id === 5 && (
+        {/* {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl" />
           </BackgroundGradientAnimation>
-        )}
+        )} */}
         <div
           className={cn(
             titleClassName,
-            " group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            " group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 "
           )}
         >
-          <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
             {description}
           </div>
-          <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200">
             {title}
           </div>
         </div>
